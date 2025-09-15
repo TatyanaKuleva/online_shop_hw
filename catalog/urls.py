60dfecc9d432aa4545dea94ecc3d6402d396c4a9
tatyana kuleva <tatianaarsentievna@gmail.com>
2025-09-15 21:03:19 +0300

    fixed urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,8 +9,8 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path("home/", ProductListView.as_view(), name="home"),
     path("contacts/", ContactPageView.as_view(), name="contacts"),
-    path("product_detail/<int:pk>", ProductDetailView.as_view(), name="product_detail"),
+    path("product_detail/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
     path("product_create/", ProductCreateView.as_view(), name="product_create"),
-    path("product_detail/<int:pk>/update", ProductUpdateView.as_view(), name="product_update"),
-    path("product_delete/<int:pk>/delete", ProductDeleteView.as_view(), name="product_delete"),
+    path("product_detail/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
+    path("product_delete/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
 ]
